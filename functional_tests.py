6581090a2b1bc69db42bf41824ceb81dd175f670
Andrea Grant <gran0260@umn.edu>
2016-08-18 12:21:24 -0500

    ftest checks that we can input an item

--- functional_tests.py
+++ functional_tests.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
-        
+
     def tearDown(self):
         self.browser.quit()
 
@@ -15,18 +16,29 @@ class NewVisitorTest(unittest.TestCase):
 
         #user sees "to-do list" in the title and header
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         #user is invited to enter an item straight away
-
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
         #user types "buy peacock feathers" into a text box
-
+        inputbox.send_keys('Buy peacock feathers')
         #when user hits enter, page updates and now says
         #"1: buy peacock feathers" as an item on a todo list
+        inputbox.send_keys(Keys.ENTER)
 
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
         #the blank text box for new entries is stil there ...
         #user types "use peacock feathers to make a fly"
-
+        self.fail('Finish the test!')
         #page updates again now with both items
 
         #how remember the list? user notices unique URL with explanatory text
