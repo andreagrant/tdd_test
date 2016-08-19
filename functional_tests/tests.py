17c595bf4dbd40786ecc3b74a73d2d8a3b0580fd
Andrea Grant <gran0260@umn.edu>
2016-08-19 10:08:25 -0500

    functional_tests is an app, use liveservertest case to clean up database

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,8 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -17,7 +17,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         #user checks out app's homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         #user sees "to-do list" in the title and header
         self.assertIn('To-Do', self.browser.title)
