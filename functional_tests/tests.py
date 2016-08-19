b68ba75281cca225c8545d465bfda978533c0e76
Andrea Grant <gran0260@umn.edu>
2016-08-19 10:20:30 -0500

    new FTs for multiple users

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -10,7 +10,7 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_tables(self, row_text):
+    def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
@@ -32,10 +32,13 @@ class NewVisitorTest(LiveServerTestCase):
         )
         #user types "buy peacock feathers" into a text box
         inputbox.send_keys('Buy peacock feathers')
-        #when user hits enter, page updates and now says
-        #"1: buy peacock feathers" as an item on a todo list
+        #when user hits enter,
+        #taken to a new URL which says
+        #"1: buy peacock feathers" as an item on a todo list table
         inputbox.send_keys(Keys.ENTER)
-        self.check_for_row_in_list_tables('1: Buy peacock feathers')
+        user1_list_url = self.browser.current_url
+        self.assertRegex(user1_list_url, '/lists/.+')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         #the blank text box for new entries is stil there ...
         #user types "use peacock feathers to make a fly"
@@ -44,15 +47,35 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 
         #page updates again now with both items
-        self.check_for_row_in_list_tables('1: Buy peacock feathers')
-        self.check_for_row_in_list_tables('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        #how remember the list? user notices unique URL with explanatory text
-        self.fail('Finish the test!')
+        #new user comes to site
 
-        #user visits direct URL and sees todo list
+        #use a new browser session to make sure no info is shared
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
 
-        #user ends session
+        #user2 visits home page ... with no sign of user1's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        #user2 starts a new list by entering an item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        #user2 gets unique URL
+        user2_list_url = self.browser.current_url
+        self.assertRegex(user2_list_url, '/lists/.+')
+        self.assertNotEqual(user2_list_url,user1_list_url)
+
+        #still no trace of user1's list
+        page_text - self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
